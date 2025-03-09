93c8a6bbf659291cf09d49066963cf64b06e2998
Luxia <e14589654@gmail.com>
2025-03-09 16:28:00 +0700

    feat: :sparkles: upload multi files

--- src/controllers/upload.ts
+++ src/controllers/upload.ts
@@ -16,6 +16,18 @@ export default async function uploadRoute(fastify: FastifyInstance) {
     },
   });
   const upload = multer({ storage });
+  const uploadFix = multer({
+    storage,
+    fileFilter: function (req, file, cb) {
+      if (file.mimetype !== "image/*") {
+        return cb(new Error("Only PNG files are allowed"), false);
+      }
+      cb(null, true);
+    },
+    limits: {
+      fileSize: 10 * 1024 * 1024, // 10MB
+    },
+  });
 
   fastify.get("/", async (request: FastifyRequest, reply: FastifyReply) => {
     return { data: "upload ready" };
@@ -25,6 +37,27 @@ export default async function uploadRoute(fastify: FastifyInstance) {
     "/file",
     {
       preHandler: upload.single("file"),
+    },
+    async (request: FastifyRequest, reply: FastifyReply) => {
+      const file = request.file as any;
+      return { data: file };
+    }
+  );
+
+  fastify.post(
+    "/files",
+    {
+      preHandler: upload.array("file", 4),
+    },
+    async (request: FastifyRequest, reply: FastifyReply) => {
+      const files = request.files as any;
+      return { data: files };
+    }
+  );
+  fastify.post(
+    "/fileFix",
+    {
+      preHandler: uploadFix.single("file"),
     },
     async (request: FastifyRequest, reply: FastifyReply) => {
       const file = request.file as any;
